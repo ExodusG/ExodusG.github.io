e113fe7f29eadb56427bd2d08e5ff5b5ee8dd86e
exodus <quentin.lelan@etudiant.univ-rennes1.fr>
2024-03-13 09:55:36 +0100

    fix cv pdf

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Quentin Le Lan | Engénieur en informatique & entrepreneur';
+export const SITE_TITLE = 'Quentin Le Lan | Ingénieur en informatique & entrepreneur';
 export const SITE_DESCRIPTION = "Etudiant ingénieur informatique à l'ESIR (Rennes), je suis spécialisé dans les systèmes d'informations. J'aime et je voudrais faire du développement web. Je reste cependant curieux et intéressé par différents domaines comme la data, le DevOps et la sécurité.";
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
