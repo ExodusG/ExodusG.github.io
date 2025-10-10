01aefc884ff024be0b25a4a90927405229eb4250
Quentin Le Lan <quentin.lelan@socleo.com>
2025-10-10 17:08:11 +0200

    add letterboxd + lastfm

---
+++ src/pages/api/lastfm.json.js
@@ -0,0 +1,44 @@
+import Parser from 'rss-parser';
+
+export const parserLastfm=new Parser({
+  customFields: {
+    item: [
+      'lfm:artist',
+      'lfm:artist_url',
+    ]
+  }
+});
+//32dd347a866c93006e15402cccd5b9fb
+
+export async function GET() {
+    try {
+        const LASTFM_API_KEY=import.meta.env.LASTFM_API_KEY;
+        const username="exodus56_";
+        console.log(LASTFM_API_KEY)
+            const topAlbum= await Promise.all([
+      fetch(`http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${username}&api_key=${LASTFM_API_KEY}&format=json&period=6month&limit=30`)
+    ]);
+
+        const topAlbumData=await topAlbum[0].json();
+
+        return Response.json({
+            topalbum:{
+                album:topAlbumData.topalbums.album.map(album=>(
+                    {
+                        name:album.name,
+                        image:album.image[2],
+                        artist:album.artist.name
+                    }
+                ))
+            }
+        })
+    } catch (error) {
+        console.error('Error fetching Letterboxd data:', error);
+        return new Response(JSON.stringify({ error: 'Failed to fetch Letterboxd data' }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+}
---
+++ src/pages/api/letterboxd.json.js
@@ -0,0 +1,61 @@
+import Parser from 'rss-parser';
+export const parserLetterboxd=new Parser({
+  customFields: {
+    item: [
+      'letterboxd:filmTitle',
+      'letterboxd:filmYear',
+      'letterboxd:memberRating',
+      'letterboxd:watchedDate',
+      'tmdb:movieId'
+    ]
+  }
+});
+export async function GET() {
+    try {
+        const feed = await parserLetterboxd.parseURL('https://letterboxd.com/exodus_/rss/');
+        
+        const movies = feed.items.map(item => {
+            const title = item['letterboxd:filmTitle'];
+            const year = item['letterboxd:filmYear'];
+            
+            let rating = 0;
+            if (item['letterboxd:memberRating']) {
+                rating = parseFloat(item['letterboxd:memberRating']);
+            }
+            
+            const imageMatch = item.content.match(/<img.*?src="(.*?)"/);
+            const image = imageMatch ? imageMatch[1] : null;
+            
+            const watchedDate = new Date(item['letterboxd:watchedDate'] || item.isoDate);
+            const correctedDate = new Date(watchedDate.getTime() + watchedDate.getTimezoneOffset() * 60000);
+
+            const formattedDate = correctedDate.toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric'
+            });
+            return {
+                title,
+                year,
+                image,
+                rating,
+                watchedDate: formattedDate,
+                link: item.link
+            };
+        });
+
+        return new Response(JSON.stringify(movies), {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (error) {
+        console.error('Error fetching Letterboxd data:', error);
+        return new Response(JSON.stringify({ error: 'Failed to fetch Letterboxd data' }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+}
