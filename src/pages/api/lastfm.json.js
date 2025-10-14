bceee885096eb9cbbd681f5016a4afa8f903d0b8
Quentin Le Lan <quentin.lelan@socleo.com>
2025-10-14 12:44:33 +0200

    up

--- src/pages/api/lastfm.json.js
+++ src/pages/api/lastfm.json.js
@@ -21,7 +21,7 @@ export async function GET() {
 
         return Response.json({
             topalbum:{
-                album:topAlbumData.topalbums.album.map(album=>(
+                album:topAlbumData?.topalbums?.album?.map(album=>(
                     {
                         name:album.name,
                         image:album.image[2],
